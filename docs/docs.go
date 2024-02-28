d142e286ea55b43c1b129c8120f5a03c1a0c384c
someone <onlyiqquee@gmail.com>
2024-02-28 14:18:53 +0100

    swagger docs updated

--- docs/docs.go
+++ docs/docs.go
@@ -76,7 +76,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "https://currency-exchange-g8p8.onrender.com",
+	Host:             "currency-exchange-g8p8.onrender.com",
 	BasePath:         "/api/v1",
 	Schemes:          []string{"https"},
 	Title:            "Cadana API",
--- readme.md
+++ readme.md
@@ -1,1 +1,5 @@
 # Currency-exchange
+
+
+- Health check: http://localhost:5002
+-Rest API endpoint: http://localhost:5002/api/v1
